2218d57d5d7f895605da540e2c54d54eec34375c
abetusk <abetusk@mechaelephant.com>
2022-11-04 04:53:25 -0400

    simple build script instead of going through the cmake/make dance

---
+++ math_belief_prop/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#g++ -O3 -I../libmin/include/ ../libmin/src/vec.cpp ../libmin/src/quaternion.cpp ../libmin/src/dataptr.cpp ../libmin/src/mersenne.cpp ../libmin/src/common_defs.cpp belief_propagation.cpp
+
+g++ -g -I../libmin/include/ ../libmin/src/vec.cpp ../libmin/src/quaternion.cpp ../libmin/src/dataptr.cpp ../libmin/src/mersenne.cpp ../libmin/src/common_defs.cpp belief_propagation.cpp
+#g++ -O3 -I../libmin/include/ ../libmin/src/vec.cpp ../libmin/src/quaternion.cpp ../libmin/src/dataptr.cpp ../libmin/src/mersenne.cpp ../libmin/src/common_defs.cpp belief_propagation.cpp
+#g++ -O2 -I../libmin/include/ ../libmin/src/vec.cpp ../libmin/src/quaternion.cpp ../libmin/src/dataptr.cpp ../libmin/src/mersenne.cpp ../libmin/src/common_defs.cpp belief_propagation.cpp
+
