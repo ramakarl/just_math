867df7093f9eaebb7dcfc2935bb94bfcd97ce2e2
abetusk <abetusk@mechaelephant.com>
2022-12-27 17:08:02 -0500

    helpful examples
    and some misc. stuff

---
+++ math_belief_prop/examples/example_run_stair.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# non-trivial run with constrained tileset
+
+./bpc -e 0.001 \
+  -I 100000  \
+  -R ./stair_rule.csv \
+  -N ./stair_name.csv \
+  -X 6 -Y 6 -Z 6 \
+  -V 1 \
+  -S 0 | tee stair.bp.6x6x6S0.log
+
+exit
+
+./bpc -e 0.00001 \
+  -I 100000  \
+  -R ./rgb_rule.csv \
+  -N ./rgb_name.csv \
+  -C ./rgb_constraint.csv \
+  -X 11 -Y 11 -Z 3 \
+  -r 512 -S 0 | tee rgb.bp.11x11x3S0.log
