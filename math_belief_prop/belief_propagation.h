509266cc8f221dd1bbb1d9c868c201cef6f76da0
Rama Karl <ramahoetzlein@gmail.com>
2023-09-04 14:34:02 -0700

    realize code flow

--- math_belief_prop/belief_propagation.h
+++ math_belief_prop/belief_propagation.h
@@ -332,7 +332,8 @@ typedef struct _bp_stat_type {
 
   float   elapsed_time;
 
-  int64_t constraints;
+  int64_t constraints; 
+  bool    success;
 
   bool    instr;
   float   time_boundary,
@@ -438,6 +439,7 @@ public:
   int       RealizePost();
   int       Realize();
 
+  int       CollapseAndPropagate (int64_t& cell, int32_t& tile, int32_t& tile_idx );
   int       CheckConstraints ( int64_t p );
   int       CheckConstraints ();
 
